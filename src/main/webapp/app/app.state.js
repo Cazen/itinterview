ff1cf500eaf1c47c0e70b2a1d19dfa692e850287
Cazen Lee <cazen@apache.org>
2016-11-22 16:52:45 +0900

    chz image file, seperate footer

--- src/main/webapp/app/app.state.js
+++ src/main/webapp/app/app.state.js
@@ -15,6 +15,9 @@
                     templateUrl: 'app/layouts/navbar/navbar.html',
                     controller: 'NavbarController',
                     controllerAs: 'vm'
+                },
+                'footer@': {
+                    templateUrl: 'app/layouts/footer/footer.html'
                 }
             },
             resolve: {
