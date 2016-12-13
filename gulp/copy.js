a916552eba4408a927abf8842893e1ac15e842d6
Cazen Lee <cazen@apache.org>
2016-12-13 05:05:30 +0000

    adjust css location

--- gulp/copy.js
+++ gulp/copy.js
@@ -41,17 +41,6 @@ function fonts() {
             base: config.dist,
             merge: true
         }))
-//        .pipe(gulp.dest(config.dist)),
-//        gulp.src(config.bower + 'bootstrap/fonts/*.*')
-//        .pipe(plumber({errorHandler: handleErrors}))
-//        .pipe(changed(config.dist + 'bower_components/bootstrap/dist/fonts/'))
-//        .pipe(flatten())
-//        .pipe(rev())
-//        .pipe(gulp.dest(config.dist + 'bower_components/bootstrap/dist/fonts/'))
-//        .pipe(rev.manifest(config.revManifest, {
-//            base: config.dist,
-//            merge: true
-//        }))
         .pipe(gulp.dest(config.dist))
     );
 }
