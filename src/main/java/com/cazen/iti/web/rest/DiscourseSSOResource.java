757ce7914c67d1fb80e8a973487d0650234b0acd
Cazen Lee <cazen@apache.org>
2017-02-05 17:42:15 +0900

    add sso with discourse

--- src/main/java/com/cazen/iti/web/rest/DiscourseSSOResource.java
+++ src/main/java/com/cazen/iti/web/rest/DiscourseSSOResource.java
@@ -76,29 +76,20 @@ public class DiscourseSSOResource {
 
         log.error("SSO Called with signedInUser: " + signedInUser.toString());
         String urlEncode = nonce
-            + "&name=" + URLEncoder.encode(signedInUser.getFirstName(), "UTF-8")
-            + "&username=" + URLEncoder.encode(signedInUser.getFirstName(), "UTF-8")
-            + "&email=" + URLEncoder.encode(signedInUser.getEmail(), "UTF-8")
+            + "&name=" + URLEncoder.encode(signedInUser.getId() == null ? "" : signedInUser.getId().toString(), "UTF-8")
+            + "&username=" + URLEncoder.encode(signedInUser.getFirstName() == null ? "" : signedInUser.getFirstName(), "UTF-8")
+            + "&email=" + URLEncoder.encode(signedInUser.getEmail() == null ? "" : signedInUser.getEmail(), "UTF-8")
             + "&external_id=" + URLEncoder.encode(signedInUser.getId() + "", "UTF-8");
         String urlBase64 = new String(Base64.encodeBase64(urlEncode.getBytes("UTF-8")));
         int length = 0;
         int maxLength = urlBase64.length();
         final int STEP = 60;
         String urlBase64Encode = "";
-        log.error("urlBase64: " + urlBase64);
-        log.error("discourseSSOLoginURL: " + discourseSSOLoginURL);
 
         while (length < maxLength) {
-            log.error("urlBase64Encode: " + urlBase64Encode);
-            log.error("length: " + length + ", STEP: " + STEP + ", maxLength: " + maxLength);
             urlBase64Encode += urlBase64.substring(length, length + STEP < maxLength ? length + STEP : maxLength) + "\n";
-            log.error("urlBase64EncodeAfter: " + urlBase64Encode);
             length += STEP;
         }
-
-        log.error("URLEncoder.encode(urlBase64Encode, UTF-8): " + URLEncoder.encode(urlBase64Encode, "UTF-8"));
-        log.error("sig: " + checksum(secretKey, urlBase64Encode));
-        log.error("discourseSSOLoginURL: " + discourseSSOLoginURL);
 
         RedirectView redirectView = new RedirectView(discourseSSOLoginURL + URLEncoder.encode(urlBase64Encode, "UTF-8") + "&sig=" +  checksum(secretKey, urlBase64Encode));
 
